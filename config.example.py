cb153547c92d1f52a6fcc0a1ca7ce0fc2d067cc5
kqzz <48300938+Kqzz@users.noreply.github.com>
2021-05-20 18:23:18 -0400

    add config.example.py and gitignore

---
+++ config.example.py
@@ -0,0 +1,18 @@
+# DB
+DATABASE = "MCsniperPY-DB"
+USER = ""
+PASSWORD = ""
+HOST = "localhost:5432"
+
+# Discord
+DISCORD_BOT_TOKEN = ""
+WEBHOOKS = [
+    {
+        "url": "",
+        "min_searches": 0
+    },
+    {
+        "url": "",
+        "min_searches": 20
+    }
+]
